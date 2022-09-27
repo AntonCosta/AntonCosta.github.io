26745407980671e8996ab0f7ea4df987598e3d9c
Costa Anton <costa.anton@tractorsetgo.com>
2022-09-27 21:51:44 +0300

    update

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-2be15603d1c5358dd317.js"
+    "url": "webpack-runtime-c8e09a043eebd7d84604.js"
   },
   {
     "url": "framework-62aa192001dd5d50f28d.js"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b1fcf88700deb3d8e286403ad84fcb14"
+    "revision": "734ed38a0283174a02eeb4ee04121fa4"
   },
   {
     "url": "polyfill-e8cfd9cfd61939dbc343.js"
